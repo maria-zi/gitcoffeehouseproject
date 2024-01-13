de12ab4f433b6c9e03ef916a3bdf4082c54db3bb
maria-zi <102587427+maria-zi@users.noreply.github.com>
2024-01-13 16:07:49 +0400

    Update create bd.sql

--- create bd.sql
+++ create bd.sql
@@ -79,6 +79,12 @@ CREATE TABLE supply (
 	 order_listid INTEGER REFERENCES orders(order_id), 
 	 product_id INTEGER REFERENCES menu(product_name)
 	 );
+	 
+	 CREATE TABLE order_staff (
+   ord_st serial PRIMARY KEY NOT NULL,
+	 id_ord INTEGER REFERENCES orders(order_id),
+	 id_staff INTEGER REFERENCES staff(staff_id)
+	 );
 
 	 
 	 
