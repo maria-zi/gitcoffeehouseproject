2df156eedb8293dd4e95650a468e5ffe5d18b001
maria-zi <102587427+maria-zi@users.noreply.github.com>
2024-01-15 18:06:52 +0400

    Update orders insert process.sql

--- orders insert process.sql
+++ orders insert process.sql
@@ -1,2 +1,3 @@
-SELECT sup_date, ing_name, sup_quantity FROM supply AS s
+SELECT sup_date, ing_name, sup_quantity, SUM(sup_quantity) AS sum FROM supply AS s
 JOIN ingredients AS i ON s.sup_ingredient = i.ing_id
+WHERE ing_name = 'milk'; 
